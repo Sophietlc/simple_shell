77efd8fff35e1256bc3fccb4eb14cf233be3232e
Sophietlc <sophiefanodun@gmail.com>
2023-02-20 14:22:25 -0800

    new

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: prints a string
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Hello there\n");
+	return (0);
+}
